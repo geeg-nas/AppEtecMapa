978b3a0cdeabb392a9e167a350dce0ad9c76821a
geeg-nas <geovananst77@gmail.com>
2023-09-20 22:31:09 -0300

    Pin no mapa

---
+++ AppRpgEtec/Views/Usuarios/LocalizacaoView.xaml.cs
@@ -0,0 +1,19 @@
+using AppRpgEtec.ViewModels.Usuarios;
+
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class LocalizacaoView : ContentPage
+{
+	LocalizacaoViewModel viewModel;
+	public LocalizacaoView()
+	{
+		InitializeComponent();
+
+		viewModel= new LocalizacaoViewModel();
+        BindingContext = viewModel;
+		//viewModel.InicializarMapa();
+		viewModel.ExibirusuariosNoMapa();
+
+		
+	}
+}
